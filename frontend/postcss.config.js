bd653de4e09c4093e59a7afcdd0b7b7f84842106
shirin44 <S3983427@rmit.edu.vn>
2025-02-16 13:15:30 +0700

    setup: Vite project with Tailwind configuration

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+// ES Module style (new)
+export default {
+    plugins: {
+      '@tailwindcss/postcss': {},
+      autoprefixer: {},
+    },
+  }
+  
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'; 
+import './App.css'
+import './index.css'
+function App() {
+
+  return (
+    <div className=" text-red-500  ">
+    this is a test 
+  </div>
+  )
+}
+
+export default App
